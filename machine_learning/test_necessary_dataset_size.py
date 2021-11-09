ed115e582a62b2f8fcc7309a2a38c8c97070b7d6
cvvletter <c.v.vletter@student.hhs.nl>
2021-11-09 11:48:22 +0100

    rudimentary dataset size test run

--- machine_learning/test_necessary_dataset_size.py
+++ machine_learning/test_necessary_dataset_size.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import svm
 
 # set the random seed so that results are reproducible
 np.random.seed(42)
@@ -11,38 +12,59 @@ features, labels = np.load('./machine_learning/features.npy'), np.load('./machin
 features, labels = np.delete(features, 102, 0), np.delete(labels, 102, 0)
 
 # print the dataset summary
-print("Features:", features.shape, "\nLabels:  ", labels.shape)
+# print("Features:", features.shape, "\nLabels:  ", labels.shape)
 
-# take subsets so the three diagnoses are separated
-periphl_subset, periphl_labels = features[0:102,:], labels[0:102]
-central_subset, central_labels = features[102:142,:], labels[102:142]
-healthy_subset, healthy_labels = features[142:202,:], labels[142:202]
-
-# print the subsets summaries to verify the subset splitting
-print(periphl_subset.shape, periphl_labels.shape)
-print(central_subset.shape, central_labels.shape)
-print(healthy_subset.shape, healthy_labels.shape)
-
-periphl_scale = len(periphl_subset)/len(features)
-central_scale = len(central_subset)/len(features)
-healthy_scale = len(healthy_subset)/len(features)
+# calculations for how many data points have to be removed each time.
+count_classes = np.bincount(labels)
+periphl_scale = count_classes[0]/len(features)
+central_scale = count_classes[1]/len(features)
+healthy_scale = count_classes[2]/len(features)
 multiplier = 0.05*len(features)
-accuracy_array = []
-size_array = []
+end_size = 0.2*len(features)
 
-while len(features) > 20:
-    periphl_subset = periphl_subset[np.random.choice(len(periphl_subset), size = len(periphl_subset) - int(round(periphl_scale*multiplier)), replace=False), :]
-    central_subset = central_subset[np.random.choice(len(central_subset), size = len(central_subset) - int(round(central_scale*multiplier)), replace=False), :]
-    healthy_subset = healthy_subset[np.random.choice(len(healthy_subset), size = len(healthy_subset) - int(round(healthy_scale*multiplier)), replace=False), :]
-    features = []
-    features.append(periphl_subset)
-    features.append(central_subset)
-    features.append(healthy_subset)
-    
-    accuracy = len(features)
-    
-    accuracy_array.append(accuracy)
-    size_array.append(len(features))
-    print(len(features))
-    
-plt.plot(size_array, accuracy_array)
+accuracy_svm = []
+# accuracy_naivebayes = []
+dataset_size = []
+
+while len(features) > end_size:
+    # print("before",len(features))
+    count_classes = np.bincount(labels)
+    for i in range (int(round(periphl_scale*multiplier))): # remove some peripheral,
+        features = np.delete(features, 0, 0)
+        labels = np.delete(labels, 0, 0)
+    for i in range (int(round(central_scale*multiplier))): # some central,
+        features = np.delete(features, 0+count_classes[0], 0)
+        labels = np.delete(labels, 0+count_classes[0], 0)
+    for i in range (int(round(healthy_scale*multiplier))): # and some healthy datapoints
+        features = np.delete(features, 0+count_classes[0]+count_classes[1], 0)
+        labels = np.delete(labels, 0+count_classes[0]+count_classes[1], 0)
+    # print("after",len(features))
+
+    # calculate accuracy for svm
+    correct = 0
+    n = len(features)
+    for i in range(len(labels)):
+        x_test = features[i]
+        x_test = np.reshape(x_test, ([1,-1]))
+        y_test = labels[i]
+        x_train_loocv = np.delete(features, i, 0)
+        y_train_loocv = np.delete(labels, i, 0)
+        modelsvm = svm.SVC(kernel='poly',degree=5,class_weight='balanced')
+        modelsvm.fit(x_train_loocv, y_train_loocv)
+        prediction = modelsvm.predict(x_test)
+        if (prediction == y_test):
+            correct += 1
+    accuracy = correct/n
+    # print("a=",accuracy)
+
+    # store accuracy...
+    accuracy_svm.append(accuracy)
+
+    # ...and dataset size
+    dataset_size.append(len(features))
+
+# plot accuracy to dataset size
+plt.plot(dataset_size, accuracy_svm)
+plt.ylabel('Testing accuracy')
+plt.xlabel('Dataset size')
+plt.show()
